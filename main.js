594d0f1a6a5ed2a476e3fa90ffba2ae7eec51fb0
cokhanh.whydah <cokhanh.duong@whydah.io>
2024-03-26 11:28:46 +0700

    Add Hand

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
+import { XRHandModelFactory } from 'three/examples/jsm/webxr/XRHandModelFactory.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
@@ -8,6 +10,10 @@ const sizes = {
   width: innerWidth,
   height: innerHeight,
 }
+
+let hand1, hand2;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
 
 const canvas = document.getElementById("webgl");
 const coitho = "https://storage.googleapis.com/assets-fygito/images/CoVatHue/CoiThoBangBac.glb"
@@ -55,6 +61,43 @@ document.body.appendChild(VRButton.createButton(renderer));
 
 const orbitControler = new OrbitControls(camera, renderer.domElement);
 orbitControler.update();
+
+controller1 = renderer.xr.getController( 0 );
+scene.add( controller1 );
+
+controller2 = renderer.xr.getController( 1 );
+scene.add( controller2 );
+
+const controllerModelFactory = new XRControllerModelFactory();
+const handModelFactory = new XRHandModelFactory();
+
+// Hand 1
+controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+scene.add( controllerGrip1 );
+
+hand1 = renderer.xr.getHand( 0 );
+hand1.add( handModelFactory.createHandModel( hand1 ) );
+
+scene.add( hand1 );
+
+// Hand 2
+controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+scene.add( controllerGrip2 );
+
+hand2 = renderer.xr.getHand( 1 );
+hand2.add( handModelFactory.createHandModel( hand2 ) );
+scene.add( hand2 );
+
+const geometryHand = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
+
+const line = new THREE.Line( geometry );
+line.name = 'line';
+line.scale.z = 5;
+
+controller1.add( line.clone() );
+controller2.add( line.clone() );
 
 // pixel ratio = 2 => per CSS pixel will be renderd by 2x2 (4pixel) on physical display device
 // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
